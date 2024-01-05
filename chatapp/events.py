61210e11b21b3d0f28785179b11d20d0e851b328
Marco <professormarco77@gmail.com>
2024-01-06 00:35:21 +0100

    created 'gameapp' folder, and worked on socket.io API on server and client side. clientAPI.js contains functions, that are well documented, to be called by the frontend through js Eventlisteners.

--- chatapp/events.py
+++ chatapp/events.py
@@ -1,9 +1,20 @@
 from flask import request
 from flask_socketio import emit
+import random
+import string
 
 from .extensions import socketio
 
 users = {}
+
+@socketio.on("create_lobby")
+def handle_create_lobby():
+    lobby_link = generate_lobby_link()
+    emit('lobby_created',{'lobby_link': lobby_link }, broadcast=True)
+
+def generate_lobby_link():
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for _ in range(20))
 
 @socketio.on("connect")
 def handle_connect():
