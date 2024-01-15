f97041c45801292a113b78981ba5fcefb9e41e75
Marco <professormarco77@gmail.com>
2024-01-15 17:13:55 +0100

    worked on user Names and how they get send to clients, it doesnt work

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from gameapp import app , io
 
-# io.run(app, host='0.0.0.0', port='5000', allow_unsafe_werkzeug=True)
-io.run(app, allow_unsafe_werkzeug=False)
+io.run(app, host='0.0.0.0', port='5000', allow_unsafe_werkzeug=False)
+#io.run(app, allow_unsafe_werkzeug=False)
