db0776d095977db7dc1c44147d8a512f199fe11a
Klaus <klausian@icloud.com>
2024-01-17 18:01:03 +0100

    added functionality to choose a username on the landing page, that is passed to the game page

--- gameapp/routes.py
+++ gameapp/routes.py
@@ -1,11 +1,19 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from gameapp import app
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        room = request.form['room']
 
-@app.route("/game/<lobbyID>")
-def game(lobbyID):
-    return render_template("game.html", room=lobbyID)
+        # return redirect('/game/{room}')
+        return redirect(url_for('game', lobbyID = room, name = name))
+    else:
+        return render_template("index.html")
+
+@app.route("/game/<lobbyID>/<name>")
+def game(lobbyID, name):
+    return render_template("game.html", room = lobbyID, name = name)
+
