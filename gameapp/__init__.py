4b3ec4fd9c5ab794633e78229aaf9091369704d9
Batuhan Gecgüner <batuhan.gecguener@tha.de>
2024-01-31 12:55:01 +0100

    datenbank initialisiert

--- gameapp/__init__.py
+++ gameapp/__init__.py
@@ -2,12 +2,18 @@ from flask import Flask
 from flask_admin import Admin
 from .events import io
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 #ruft config.py auf
 app.config.from_object('config')
+
+class Base(DeclarativeBase):
+    pass
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 io.init_app(app)
 
