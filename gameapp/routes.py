9837dfadd77428c852a9315f9b33e18fb2ace79c
Marco <professormarco77@gmail.com>
2024-02-03 02:48:05 +0100

    added sanitizer for injection into db

--- gameapp/routes.py
+++ gameapp/routes.py
@@ -1,15 +1,15 @@
 from flask import render_template, request, redirect, url_for
 from gameapp import app, db
-import string, random
+import string, random, re
 from flask_security import current_user
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        name = request.form['name']
+        name = sanitize_input(request.form['name'])
         current_user.username = name
         db.session.commit()
-        room = request.form['room']
+        room = sanitize_input(request.form['room'])
 
         if room == "" or room == None:
             room = createLobbyLink()
@@ -46,13 +46,18 @@ def game(lobbyID, name):
                                 name = name, 
                                 isAuthenticated = "false")
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 def createLobbyLink():
     characters = string.ascii_letters + string.digits
     random_string = ''.join(random.choice(characters) for i in range(8))
     return random_string
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+def sanitize_input(input_string):
+    allowed_characters = re.compile(r'[a-zA-Z0-9_\-.,\s]+')
+    sanitized_string = ''.join(allowed_characters.findall(input_string))
 
+    return sanitized_string
 
